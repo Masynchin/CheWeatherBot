b17945987485071a1ee43a50f9a942adcbebe28b
Masynchin <banzur.badurov@gmail.com>
2021-03-18 23:43:39 +0300

    fix encoding in logger

--- logger.py
+++ logger.py
@@ -2,8 +2,9 @@ from loguru import logger
 
 
 logger.add(
-    sink='logs/log.log',
+    encoding="u8",
+    sink="logs/log.log",
     format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}",
-    rotation='1 week',
-    compression='zip',
+    rotation="1 week",
+    compression="zip",
 )
