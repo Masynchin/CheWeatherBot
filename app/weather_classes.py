f69551049860b6ec0c15f9fa5511004edbca7ce9
Masynchin <masynchin@gmail.com>
2021-07-19 02:40:17 +0300

    cover all classes and functions in weather_classes.py with docstrings

--- app/weather_classes.py
+++ app/weather_classes.py
@@ -1,7 +1,10 @@
 """Модуль с классом обработки ответа с погодного сервера.
 
-Содержит основной класс - WeatherResponse, которий используется
-для получения сводки погоды в виде текста
+Содержит основной класс - WeatherResponse, который преобразует JSON ответ API
+в python-структуру.
+
+Ссылка на пример ответа API:
+https://openweathermap.org/api/one-call-api#example
 """
 
 import datetime as dt
@@ -15,19 +18,53 @@ import utils
 
 
 class WeatherDescription(BaseModel):
+    """Представление описания погоды.
+
+    Структура в API:
+    {
+      "main": str  ("Clouds", "Rain", ...)
+      "description": str  ("few clouds", ...)
+      ...
+    }
+    """
+
     main: str
     description: str
 
     @validator("description")
     def capitalize_description(cls, description):
+        """Добавляем заглавную букву в поле `description`"""
         return description.capitalize()
 
 
 def _decorate_temp(temp: float) -> str:
+    """Превращаем численное значение температуры в строку.
+
+    -10 -> "-10°"
+    10 -> "+10°"
+    """
     return f"+{temp}°" if temp > 0 else f"{temp}°"
 
 
 class BaseWeather(BaseModel):
+    """Базовое представление погоды.
+
+    Базовый класс представления погоды, от которого наследуются конкретные
+    для своего времени представления (представление для текущей погоды,
+    дневной погоды и т.д.).
+
+    Структура в API:
+    {
+      "dt": int (UTC timestamp) (1618317040, ...)
+      "humidity": int  (62, ...)
+      "clouds": int  (0, ...)
+      "wind_speed": float  (6.0, ...)
+      "wind_gust": float?  (10.2, ...)
+      "weather": [WeatherDescription]
+      ...
+    }
+    """
+
     timestamp: dt.datetime = Field(alias="dt")
     humidity: int
     cloudiness: int = Field(alias="clouds")
@@ -37,18 +74,42 @@ class BaseWeather(BaseModel):
 
     @validator("wind_speed", "wind_gust")
     def decorate_temperate(cls, wind):
+        """Превращаем численное значение скорости ветра в строку.
+
+        10.2 -> "10.2 м/с"
+        """
         return f"{wind} м/с"
 
     @validator("cloudiness", "humidity")
     def decorate_percentage(cls, percentage):
+        """Превращаем численное значение процентов в строку.
+
+        10 -> "10%"
+        """
         return f"{percentage}%"
 
     @validator("weather_type")
     def first_element_from_list(cls, weather_type):
+        """Используем только первое описание погоды.
+
+        Достаём из массива значений поля `weather_type` (алиас к `weather`)
+        только первый элемент
+        """
         return weather_type[0]
 
 
 class Weather(BaseWeather):
+    """Представление погоды.
+
+    Структура в API:
+    {
+      ...
+      "temp": float  (20.72, ...)
+      "feels_like": float  (19.33, ...)
+      ...
+    }
+    """
+
     temp: float
     feels_like: float
 
@@ -58,6 +119,19 @@ class Weather(BaseWeather):
 
 
 class DailyTemperature(BaseModel):
+    """Представление действительной температуры для дневной погоды.
+
+    Структура в API:
+    {
+      "day": float  (26.78, ...)
+      "min": float  (13.92, ...)
+      "max": float  (27.49, ...)
+      "night": float  (16.78, ...)
+      "eve": float  (25.41, ...)
+      "morn": float  (18.2, ...)
+    }
+    """
+
     morn_temp: float = Field(alias="morn")
     day_temp: float = Field(alias="day")
     eve_temp: float = Field(alias="eve")
@@ -77,6 +151,17 @@ class DailyTemperature(BaseModel):
 
 
 class DailyFeelsLike(BaseModel):
+    """Представление ощущаемой температуры для дневной погоды.
+
+    Структура в API:
+    {
+      "day": float  (26.75, ...)
+      "night": float  (15.85, ...)
+      "eve": float  (25.02, ...)
+      "morn": float  (18.24, ...)
+    }
+    """
+
     morn_feels_like: float = Field(alias="morn")
     day_feels_like: float = Field(alias="day")
     eve_feels_like: float = Field(alias="eve")
@@ -92,21 +177,43 @@ class DailyFeelsLike(BaseModel):
 
 
 class DailyWeather(BaseWeather):
+    """Представление дневной погоды.
+
+    Структура в API:
+    {
+      ...
+      "temp": DailyTemperature
+      "feels_like": DailyFeelsLike
+      ...
+    }
+    """
+
     temp: DailyTemperature
     feels_like: DailyFeelsLike
 
 
 class Alert(BaseModel):
+    """Представление предупреждения.
+
+    Структура в API:
+    {
+      "event": str  ("Ветер", ...)
+      "description": str  ("местами порывы 15-20 м/с", ...)
+      ...
+    }
+    """
+
     event: str
     description: str
 
 
 def _is_english_alert(alert):
+    """Написано ли предупреждение на английском"""
     return bool(set(alert.event) & set(ascii_letters))
 
 
 class WeatherResponse(BaseModel):
-    """Класс, преобразующий ответ с погодного сайта в удобный формат.
+    """Класс, преобразующий ответ с погодного сайта в python-структуру.
 
     Предназначен для получения сводки погоды/прогноза в виде текста.
     Предоставляет числовые данные - температура, ветренность, облачность,
@@ -120,11 +227,13 @@ class WeatherResponse(BaseModel):
 
     @validator("alerts")
     def filter_alerts(cls, alerts):
+        """Оставляем только предупрежденя написанные на русском"""
         if alerts is None:
             return []
         return [alert for alert in alerts if not _is_english_alert(alert)]
 
     def get_current_weather(self):
+        """Получение текущей погоды"""
         text = (
             templates.WEATHER_WITH_WIND_GUST
             if self.current.wind_gust is not None
@@ -136,6 +245,7 @@ class WeatherResponse(BaseModel):
         return text, weather_type
 
     def get_hourly_forecast(self):
+        """Получение погоды в ближайший час"""
         forecast = self._get_nearest_hour_forecast()
         text = (
             templates.WEATHER_WITH_WIND_GUST
@@ -148,12 +258,14 @@ class WeatherResponse(BaseModel):
         return text, weather_type
 
     def _get_nearest_hour_forecast(self):
+        """Получение данных о погоде в ближайший час"""
         now = utils.get_current_time()
         future_forecasts = filter(lambda f: f.timestamp > now, self.hourly)
         nearest_forecast = min(future_forecasts, key=lambda f: f.timestamp)
         return nearest_forecast
 
     def get_exact_hour_forecast(self, hour):
+        """Получение прогноза погоды в конкретный час"""
         forecast = self._get_exact_hour_forecast(hour)
         text = (
             templates.WEATHER_WITH_WIND_GUST
@@ -166,10 +278,12 @@ class WeatherResponse(BaseModel):
         return text, weather_type
 
     def _get_exact_hour_forecast(self, hour):
+        """Получение данных о прогнозе погоды в конкретный час"""
         forecast = [f for f in self.hourly if f.timestamp == hour][0]
         return forecast
 
     def get_daily_forecast(self):
+        """Получение прогноза на ближайший день"""
         forecast = self._get_nearest_daily_forecast()
         text = (
             templates.DAILY_FORECAST_WITH_WIND_GUST
@@ -182,12 +296,14 @@ class WeatherResponse(BaseModel):
         return text, weather_type
 
     def _get_nearest_daily_forecast(self):
+        """Получение данных о прогнозе на ближайший день"""
         now = utils.get_current_time()
         future_forecasts = filter(lambda f: f.timestamp > now, self.daily)
         nearest_forecast = min(future_forecasts, key=lambda f: f.timestamp)
         return nearest_forecast
 
     def get_exact_day_forecast(self, day):
+        """Получение прогноза на конкретный день"""
         forecast = self._get_exact_day_forecast(day)
         text = (
             templates.DAILY_FORECAST_WITH_WIND_GUST
@@ -200,10 +316,12 @@ class WeatherResponse(BaseModel):
         return text, weather_type
 
     def _get_exact_day_forecast(self, day):
+        """Получение данных о прогнозе на конкретный день"""
         forecast = [f for f in self.daily if f.timestamp.date() == day.date()][0]
         return forecast
 
     def _generate_alert_text(self):
+        """Генерация текста с предупреждениями"""
         if not self.alerts:
             return ""
         return "\n\n" + "\n".join(
