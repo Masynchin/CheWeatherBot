d474c3c0bb43f85bf890e9f7d59b3ee71a00d14a
Max Smirnov <masynchin@gmail.com>
2023-07-17 12:28:00 +0300

    Handle BotBlocked in mailing

--- app/mailing.py
+++ app/mailing.py
@@ -3,6 +3,8 @@
 Каждые 15 минут происходит рассылка всем её подписчикам
 """
 
+from aiogram.exceptions import TelegramForbiddenError
+
 from app import templates
 from app.logger import logger
 
@@ -14,23 +16,32 @@ async def mailing(bot, db, weather, mailing_times):
     данным временем, и каждому отправляет прогноз погоды
     """
     async for mailing_time in mailing_times:
-        await send_mailing(bot, db, weather, mailing_time.time())
+        await send_mailings(bot, db, weather, mailing_time.time())
 
 
-async def send_mailing(bot, db, weather, mailing_time):
+async def send_mailings(bot, db, weather, mailing_time):
     """Отправляем рассылку пользователям с данным временем"""
     forecast = await weather.current()
     message_text = templates.MAILING_MESSAGE.format(forecast.format())
     sticker = forecast.sticker()
     subscribers = await db.of_time(mailing_time)
-
     for subscriber in subscribers:
         user_id = subscriber.id
-        await bot.send_sticker(user_id, sticker)
-        message = await bot.send_message(user_id, message_text)
-        await unpin_all_and_pin_message(bot, message)
+        try:
+            await send_mailing(bot, user_id, message_text, sticker)
+            logger.info(f"Пользователь {user_id} получил ежедневный прогноз")
+        except TelegramForbiddenError:
+            await db.delete(user_id)
+            logger.info(
+                f"Пользователь {user_id} удалён из рассылки из-за блока бота"
+            )
 
-        logger.info(f"Пользователь {user_id} получил ежедневный прогноз")
+
+async def send_mailing(bot, user_id, message_text, sticker):
+    """Отправляем рассылку пользователю"""
+    await bot.send_sticker(user_id, sticker)
+    message = await bot.send_message(user_id, message_text)
+    await unpin_all_and_pin_message(bot, message)
 
 
 async def unpin_all_and_pin_message(bot, message):
